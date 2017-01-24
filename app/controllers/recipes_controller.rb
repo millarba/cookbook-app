9997e116db8ad32e368267f9a9dee206c3e4ab0e
millarba <millarba@gmail.com>
2017-01-23 21:03:46 -0600

    refactoring

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,24 +1,72 @@
 class RecipesController < ApplicationController
 
-  def one_recipe
-    @recipe = Recipe.last
-  end
+  # def one_recipe
+  #   @recipe = Recipe.last
+  # end
 
-  def all_recipes
+  # def all_recipes
+  #   @recipes = Recipe.all
+  # end
+
+  # def recipe_form
+
+  # end
+
+  # def recipe_success
+  #   @recipe = Recipe.new(
+  #                         title: params["title"],
+  #                         chef: params["chef"],
+  #                         ingredients: params["ingredients"],
+  #                         directions: params["directions"],
+  #                         )
+  #   @recipe.save
+  # end
+  def index
     @recipes = Recipe.all
   end
 
-  def recipe_form
+  def new
 
   end
 
-  def recipe_success
-    @recipe = Recipe.new(
-                          title: params["title"],
-                          chef: params["chef"],
-                          ingredients: params["ingredients"],
-                          directions: params["directions"]
+  def create
+    recipe = Recipe.new(
+                          title: params[:title],
+                          chef: params[:chef],
+                          ingredients: params[:ingredients],
+                          directions: params[:directions]
                           )
-    @recipe.save
+    recipe.save
+    flash[:success] = "Recipe Created Successfully."
+    redirect_to "/recipes/#{recipe.id}"
   end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title]
+    @recipe.chef = params[:chef]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.directions = params[:directions]
+
+    @recipe.save
+    flash[:info] = "Recipe Updated Successfully."
+    redirect_to "/recipes/#{@recipe.id}"
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    flash[:warning] = "Recipe Removed Successfully."
+    redirect_to "/recipes"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  get '/one_recipe_url' => 'recipes#one_recipe'
-  get '/all_recipes' => 'recipes#all_recipes'
+  # get '/one_recipe_url' => 'recipes#one_recipe'
+  # get '/all_recipes' => 'recipes#all_recipes'
 
-  get '/recipe_form' => 'recipes#recipe_form'
-  post '/recipe_form' => 'recipes#recipe_success'
+  # get '/recipe_form' => 'recipes#recipe_form'
+  # post '/recipe_form' => 'recipes#recipe_success'
+
+  get '/' => 'recipes#index'
+  get '/recipes' => 'recipes#index'
+
+  get '/recipes/new' => 'recipes#new'
+  post '/recipes' => 'recipes#create'
+
+  get '/recipes/:id' => 'recipes#show'
+
+  get '/recipes/:id/edit' => 'recipes#edit'
+  patch '/recipes/:id' => 'recipes#update'
+
+  delete '/recipes/:id' => 'recipes#destroy'
 end
